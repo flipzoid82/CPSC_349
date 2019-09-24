57dc585565356280bad6bb5eeef17357889c3acb
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-09-24 11:50:28 -0700

    Coffeerun Updates

--- hw3/coffeerun/scripts/formhandler.js
+++ hw3/coffeerun/scripts/formhandler.js
@@ -14,19 +14,28 @@
   }
 
   FormHandler.prototype.addSubmitHandler = function(fn) {
+    /* eslint-disable-next-line no-console */
     console.log('Setting submit handler for form');
     this.$formElement.on('submit', function(event) {
       event.preventDefault();
       var data = {};
       $(this).serializeArray().forEach(function(item) {
         data[item.name] = item.value;
+        /* eslint-disable-next-line no-console */
         console.log(item.name + ' is ' + item.value);
       });
+      /* eslint-disable-next-line no-console */
       console.log(data);
       fn(data);
       this.reset();
       this.elements[0].focus();
     });
+  };
+
+  FormHandler.prototype.submitHandler = function() {
+    /* eslint-disable-next-line no-console */
+    console.log('Setting submit handler for form');
+    // More code will go here
   };
 
   App.FormHandler = FormHandler;
