ba64acf86bcb31c8ac920311f512fde335c1d6b3
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-10-05 15:29:30 -0700

    ch13-ajax updates

--- hw4/coffeerun/scripts/formhandler.js
+++ hw4/coffeerun/scripts/formhandler.js
@@ -33,6 +33,21 @@
     });
   };
 
+  FormHandler.prototype.addInputHandler = function(fn) {
+    /* eslint-disable-next-line no-console */
+    console.log('Setting input handler for form');
+    this.$formElement.on('input', '[name="emailAddress"]', function(event) {
+      var emailAddress = event.target.value;
+      var message = '';
+      if (fn(emailAddress)) {
+        event.target.setCustomValidity('');
+      } else {
+        message = emailAddress + ' is not an authorized email address!'
+        event.target.setCustomValidity(message);
+      }
+    });
+  };
+
   App.FormHandler = FormHandler;
   window.App = App;
 })(window);
