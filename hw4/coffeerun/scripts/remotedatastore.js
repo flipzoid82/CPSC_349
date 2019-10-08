9e7e9e8b2aa934ee0a9916a9fe45133bce63176f
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-10-08 00:40:09 -0700

    RemoteDataSores.js updates

--- hw4/coffeerun/scripts/remotedatastore.js
+++ hw4/coffeerun/scripts/remotedatastore.js
@@ -2,6 +2,10 @@
   'use strict';
   var App = window.App || {};
   var $ = window.jQuery;
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var CheckList = App.CheckList;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  var ID;
 
   function RemoteDataStore(url) {
     if (!url) {
@@ -17,24 +21,35 @@
     });
   };
 
-  RemoteDataStore.prototype.getAll = function(cb) {
+  RemoteDataStore.prototype.getAll = function() {
     $.get(this.serverUrl, function(serverResponse) {
       /* eslint-disable-next-line no-console */
       console.log(serverResponse);
-      cb;
+      serverResponse.forEach(function(response){
+        checkList.addRow.call(checkList, response);
+      });
+      //cb(serverResponse);
     });
   };
 
   RemoteDataStore.prototype.get = function(key, cb) {
-    $.get(this.serverUrl + '/' + key, function(serverResponse) {
+    $.get(this.serverUrl + '?emailAddress=' + key, function(serverResponse) {
       /* eslint-disable-next-line no-console */
-      console.log(serverResponse);
-      cb(serverResponse);
+      //console.log(serverResponse);
+      //var obj = JSON.stringify(serverResponse);
+      ID = serverResponse[0].id;
+      //console.log('test');
+      console.log(ID);
     });
   };
 
+var remoteDS = new RemoteDataStore('http://localhost:2403/coffeeorders');
+
   RemoteDataStore.prototype.remove = function(key) {
-    $.ajax(this.serverUrl + '/' + key, {
+    remoteDS.get(key);
+    console.log('remove test');
+    console.log(ID);
+    $.ajax(this.serverUrl + '/' + ID, {
       type: 'DELETE'
     });
   };
